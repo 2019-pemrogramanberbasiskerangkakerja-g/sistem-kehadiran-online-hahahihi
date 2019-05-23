f5c6ee80839181e196e34c8fcff3490b83d72e20
Hilmi Raditya <raditya113@icloud.com>
2019-05-23 22:30:47 +0700

    revisi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+var validator = require('validator');
 var timestamp = require('time-stamp');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,41 +17,65 @@ var con = mysql.createConnection({
 
 app.get('/', function(req,res){
   console.log(timestamp('YYYY-MM-DD HH:mm:ss'));
-  res.send(" API PBKK - Kelompok 5 - Naufal - Akram - Hilmi ");
+  res.send("hello world");
 });
 
 app.post('/tambahmatkul', function (req, res) {
-  if(req.body.nama == null || req.body.semester == null || req.body.kelas == null){
+  if(req.body.nama == null || req.body.semester == null || req.body.kelas == null || req.body.kode_matkul == null){
     res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
   }
-  con.connect(function(err) {
-    console.log("Connected!");
-    var sql = "INSERT INTO matakuliah (nama, jumlah_pertemuan, semester, kelas) VALUES ?";
-    var values = [
-      [req.body.nama, 16, req.body.semester, req.body.kelas]
-    ];
-    con.query(sql, [values], function (err, result) {
-      console.log("data berhasil masuk");
+  else if(validator.isEmpty(req.body.nama) || 
+    validator.isEmpty(req.body.semester) || 
+    validator.isEmpty(req.body.kelas) ||
+    validator.isEmpty(req.body.kode_matkul)){
+      res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
+  }
+  else{
+    con.connect(function(err) {
+      con.query("SELECT * FROM matakuliah where kode_matkul = "+req.body.kode_matkul, function (err, result) {
+        if(result.length > 0){
+          res.send({status : "gagal", pesan : "kode matakuliah telah terdaftar", isi_data : req.body });      
+        }
+        else{
+          var sql = "INSERT INTO matakuliah (nama, jumlah_pertemuan, semester, kelas) VALUES ?";
+          var values = [
+            [req.body.nama, 16, req.body.semester, req.body.kelas]
+          ];
+          con.query(sql, [values], function (err, result) {
+            console.log("data berhasil masuk");
+          });
+          res.send({status : "sukses", pesan : "data berhasil masuk", isi_data : req.body });
+        }
+      });
     });
-  });
-  res.send({status : "sukses", pesan : "data berhasil masuk", isi_data : req.body });
+  }
 });
 
 app.post('/tambahjadwal', function (req, res) {
   if(req.body.pertemuan == null || req.body.jam_mulai == null || req.body.jam_selesai == null || req.body.ruangan == null || req.body.matakuliah_id == null){
     res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
   }
-  con.connect(function(err) {
-    console.log("Connected!");
-    var sql = "INSERT INTO  pertemuan_matakuliah (pertemuan, jam_mulai, jam_selesai, ruangan, matakuliah_id) VALUES ?";
-    var values = [
-      [req.body.pertemuan, req.body.jam_mulai, req.body.jam_selesai, req.body.ruangan, req.body.matakuliah_id]
-    ];
-    con.query(sql, [values], function (err, result) {
-      console.log("data berhasil masuk");
+  else if(validator.isEmpty(req.body.pertemuan) || 
+    validator.isEmpty(req.body.jam_mulai) || 
+    validator.isEmpty(req.body.jam_selesai) ||
+    validator.isEmpty(req.body.ruangan) ||
+    validator.isEmpty(req.body.matakuliah_id)
+  ){
+    res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
+  }
+  else{
+    con.connect(function(err) {
+      console.log("Connected!");
+      var sql = "INSERT INTO  pertemuan_matakuliah (pertemuan, jam_mulai, jam_selesai, ruangan, matakuliah_id) VALUES ?";
+      var values = [
+        [req.body.pertemuan, req.body.jam_mulai, req.body.jam_selesai, req.body.ruangan, req.body.matakuliah_id]
+      ];
+      con.query(sql, [values], function (err, result) {
+        console.log("data berhasil masuk");
+      });
     });
-  });
-  res.send({status : "sukses", pesan : "data berhasil masuk", isi_data : req.body });
+    res.send({status : "sukses", pesan : "data berhasil masuk", isi_data : req.body });
+  }
 });
 
 
@@ -58,26 +83,34 @@ app.post('/tambahmahasiswa', function (req, res) {
   if(req.body.nama == null || req.body.nrp == null || req.body.password == null){
     res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
   }
-  con.connect(function(err){
-    var sql_cek = "SELECT * FROM mahasiswa where nrp="+req.body.nrp;
-    con.query(sql_cek, function (err, result) {
-      console.log(result);
-      if(result.length > 0){
-        res.send({status : "gagal", pesan : "nrp telah terdaftar", isi_data : req.body });
-      }
-      else{
-        var sql = "INSERT INTO  mahasiswa (nrp, nama, password) VALUES ?";
-        var values = [req.body.nrp, req.body.nama, req.body.password];
-        con.query(sql, [values], function (err, result) {
-          res.send({status : "sukses", pesan : "data berhasil masuk", isi_data : req.body });
-        });
-      }
-    });    
-  });
+  else if(validator.isEmpty(req.body.nama) || validator.isEmpty(req.body.nrp) || validator.isEmpty(req.body.password)){
+    res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
+  }
+  else{
+    con.connect(function(err){
+      var sql_cek = "SELECT * FROM mahasiswa where nrp="+req.body.nrp;
+      con.query(sql_cek, function (err, result) {
+        console.log(result);
+        if(result.length > 0){
+          res.send({status : "gagal", pesan : "nrp telah terdaftar", isi_data : req.body });
+        }
+        else{
+          var sql = "INSERT INTO  mahasiswa (nrp, nama, password) VALUES ?";
+          var values = [req.body.nrp, req.body.nama, req.body.password];
+          con.query(sql, [values], function (err, result) {
+            res.send({status : "sukses", pesan : "data berhasil masuk", isi_data : req.body });
+          });
+        }
+      });    
+    });
+  }
 });
 
 app.post('/tambahpesertakelas', function(req,res){
-  if(req.body.nrp == null || req.body.id_matkul == null){
+  if(req.body.nrp == null || req.body.kode_matkul == null){
+    res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
+  }
+  else if(validator.isEmpty(req.body.nrp) || validator.isEmpty(req.body.kode_matkul)){
     res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
   }
   else{
@@ -88,7 +121,7 @@ app.post('/tambahpesertakelas', function(req,res){
           res.send({status : "gagal", pesan : "nrp tidak terdaftar", isi_data : req.body });
         }
         else{
-          var cek_sql = "SELECT * FROM ambil_matakuliah WHERE matakuliah_id = "+req.body.id_matkul+" AND mahasiswa_id = (SELECT id FROM mahasiswa WHERE nrp = "+req.body.nrp+");";
+          var cek_sql = "SELECT * FROM ambil_matakuliah WHERE matakuliah_id = (SELECT id FROM matakuliah where kode_matkul = "+req.body.kode_matkul+") AND mahasiswa_id = (SELECT id FROM mahasiswa WHERE nrp = "+req.body.nrp+");";
           con.query(cek_sql, function (error, results, fields) {
             if (error) throw error;
             console.log(results)
@@ -96,7 +129,7 @@ app.post('/tambahpesertakelas', function(req,res){
               res.send({status : "gagal", pesan: "mahasiswa telah mendaftar matakuliah ini", isi_data : req.body});
             }
             else{
-              var sql = "INSERT INTO ambil_matakuliah(matakuliah_id,mahasiswa_id) VALUES ("+req.body.id_matkul+",(SELECT id FROM mahasiswa WHERE nrp = "+req.body.nrp+"))";
+              var sql = "INSERT INTO ambil_matakuliah(matakuliah_id,mahasiswa_id) VALUES ((SELECT id FROM matakuliah WHERE kode_matkul = "+req.body.kode_matkul+"),(SELECT id FROM mahasiswa WHERE nrp = "+req.body.nrp+"))";
               con.query(sql, function (error, results, fields) {
                 if (error) throw error;
                 console.log("data berhasil masuk");
@@ -114,50 +147,55 @@ app.post('/absen', function (req, res) {
   if(req.body.nrp == null || req.body.ruangan == null){
     res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
   }
-  con.connect(function(err) {
-    console.log("Connected!");
-    con.query("SELECT * FROM mahasiswa where NRP ="+req.body.nrp, function(error, results, fields){
-      if(results.length == 0){
-        res.send({status : "gagal", pesan : "nrp tidak terdaftar", isi_data : req.body });
-      }
-      else{
-        var query_check = "SELECT matakuliah_id FROM pertemuan_matakuliah WHERE ruangan = '"+req.body.ruangan+"' AND jam_mulai <= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"' AND jam_selesai >= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"';";
-        con.query(query_check, function(error, results_matakuliahid, fields){
-          if(results_matakuliahid.length == 0){
-            res.send({status : "gagal", pesan : "tidak ada matakuliah yang terdaftar", isi_data : req.body });
-          }
-          else{
-            var query_cek_absen = "SELECT * FROM absensi WHERE mahasiswa_id=(SELECT id FROM mahasiswa where NRP='"+req.body.nrp+"') AND pertemuan_matakuliah_id=(SELECT id FROM pertemuan_matakuliah WHERE ruangan = '"+req.body.ruangan+"' AND jam_mulai <= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"' AND jam_selesai >= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"');";
-            con.query(query_cek_absen, function(error, results, fields){
-              if(results.length > 0){
-                res.send({status : "gagal", pesan : "peserta telah absen", isi_data : req.body });
-              }
-              else{
-                var query = "INSERT INTO absensi(mahasiswa_id, tanggal_waktu, pertemuan_matakuliah_id) VALUES((SELECT id FROM mahasiswa where NRP='"+req.body.nrp+"'),'"+timestamp('YYYY-MM-DD HH:mm:ss')+"',(SELECT id FROM pertemuan_matakuliah WHERE ruangan = '"+req.body.ruangan+"' AND jam_mulai <= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"' AND jam_selesai >= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"'));";
-                con.query(query, function(error, results, fields){
-                  if(error) console.log(error);
-                  res.send({status : "berhasil", pesan : "absen berhasil", isi_data : req.body });
-                });
-              }
-            });
-          }
-        });
-      }
+  else if(validator.isEmpty(req.body.nrp) || validator.isEmpty(req.body.ruangan)){
+    res.send({status : "gagal", pesan : "data ada yang tidak lengkap", isi_data : req.body });
+  }
+  else{
+    con.connect(function(err) {
+      console.log("Connected!");
+      con.query("SELECT * FROM mahasiswa where NRP ="+req.body.nrp, function(error, results, fields){
+        if(results.length == 0){
+          res.send({status : "gagal", pesan : "nrp tidak terdaftar", isi_data : req.body });
+        }
+        else{
+          var query_check = "SELECT matakuliah_id FROM pertemuan_matakuliah WHERE ruangan = '"+req.body.ruangan+"' AND jam_mulai <= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"' AND jam_selesai >= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"';";
+          con.query(query_check, function(error, results_matakuliahid, fields){
+            if(results_matakuliahid.length == 0){
+              res.send({status : "gagal", pesan : "tidak ada matakuliah yang terdaftar", isi_data : req.body });
+            }
+            else{
+              var query_cek_absen = "SELECT * FROM absensi WHERE mahasiswa_id=(SELECT id FROM mahasiswa where NRP='"+req.body.nrp+"') AND pertemuan_matakuliah_id=(SELECT id FROM pertemuan_matakuliah WHERE ruangan = '"+req.body.ruangan+"' AND jam_mulai <= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"' AND jam_selesai >= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"');";
+              con.query(query_cek_absen, function(error, results, fields){
+                if(results.length > 0){
+                  res.send({status : "gagal", pesan : "peserta telah absen", isi_data : req.body });
+                }
+                else{
+                  var query = "INSERT INTO absensi(mahasiswa_id, tanggal_waktu, pertemuan_matakuliah_id) VALUES((SELECT id FROM mahasiswa where NRP='"+req.body.nrp+"'),'"+timestamp('YYYY-MM-DD HH:mm:ss')+"',(SELECT id FROM pertemuan_matakuliah WHERE ruangan = '"+req.body.ruangan+"' AND jam_mulai <= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"' AND jam_selesai >= '"+timestamp('YYYY-MM-DD HH:mm:ss')+"'));";
+                  con.query(query, function(error, results, fields){
+                    if(error) console.log(error);
+                    res.send({status : "berhasil", pesan : "absen berhasil", isi_data : req.body });
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
     });
-  });
+  }
 });
 
-app.get('/rekap/:id_matkul', function (req, res) {
+app.get('/rekap/:kode_matkul', function (req, res) {
   con.connect(function(err) {
-    con.query("SELECT * FROM MAHASISWA WHERE id in (SELECT mahasiswa_id FROM ambil_matakuliah WHERE matakuliah_id = '"+req.params.id_matkul+"');", function (err, result) {
+    con.query("SELECT * FROM MAHASISWA WHERE id in (SELECT mahasiswa_id FROM ambil_matakuliah WHERE matakuliah_id = (SELECT id FROM matakuliah where kode_matkul = '"+req.params.kode_matkul+"'));", function (err, result) {
       res.send({status : "sukses", pesan : "rekap peserta mata kuliah :", isi_data : result });
     });
   });
 });
 
-app.get('/rekap/:id_matkul/:pertemuan', function (req, res) {
+app.get('/rekap/:kode_matkul/:pertemuan', function (req, res) {
   con.connect(function(err) {
-    con.query("SELECT * FROM absensi WHERE mahasiswa_id IN (SELECT mahasiswa_id FROM pertemuan_matakuliah WHERE matakuliah_id = '"+req.params.id_matkul+"' AND pertemuan = '"+req.params.pertemuan+"')" , function (err, result) {
+    con.query("SELECT * FROM absensi WHERE mahasiswa_id IN (SELECT mahasiswa_id FROM pertemuan_matakuliah WHERE matakuliah_id = (SELECT id FROM matakuliah WHERE kode_matkul = '"+req.params.kode_matkul+"') AND pertemuan = '"+req.params.pertemuan+"')" , function (err, result) {
       res.send({status : "sukses", pesan : "rekap peserta mata kuliah :", isi_data : result });
     });
   });
@@ -171,10 +209,26 @@ app.get('/rekapmahasiswasemester/:nrp/:semester', function (req, res) {
   });
 });
 
-app.get('/rekapmahasiswa/:nrp/:id_matkul', function (req, res) {
+app.get('/rekapmahasiswa/:nrp/:kode_matkul', function (req, res) {
   con.connect(function(err) {
-    con.query("SELECT * FROM absensi WHERE mahasiswa_id IN (SELECT id FROM mahasiswa WHERE nrp = '"+req.params.nrp+"') AND pertemuan_matakuliah_id IN (SELECT id FROM pertemuan_matakuliah where matakuliah_id="+req.params.id_matkul+");" , function (err, result) {
+    con.query("SELECT * FROM absensi WHERE mahasiswa_id IN (SELECT id FROM mahasiswa WHERE nrp = '"+req.params.nrp+"') AND pertemuan_matakuliah_id IN (SELECT id FROM pertemuan_matakuliah where matakuliah_id=(SELECT id FROM matakuliah WHERE kode_matkul = '"+req.params.kode_matkul+"'));" , function (err, result) {
       res.send({status : "sukses", pesan : "rekap peserta mata kuliah :", isi_data : result });
+    });
+  });
+});
+
+app.get('/mahasiswa', function(req,res){
+  con.connect(function(err) {
+    con.query("SELECT * FROM mahasiswa;" , function (err, result) {
+      res.send({status : "sukses", pesan : "rekap mahasiswa :", isi_data : result });
+    });
+  });
+});
+
+app.get('/matakuliah', function(req,res){
+  con.connect(function(err) {
+    con.query("SELECT * FROM matakuliah;" , function (err, result) {
+      res.send({status : "sukses", pesan : "rekap matakuliah :", isi_data : result });
     });
   });
 });
--- tugas5pbkk (2).sql
+++ tugas5pbkk (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 18, 2019 at 04:09 PM
+-- Generation Time: May 23, 2019 at 05:17 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -31,24 +31,37 @@ SET time_zone = "+00:00";
 CREATE TABLE `absensi` (
   `id` int(11) NOT NULL,
   `tanggal_waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `pertemuan` int(11) NOT NULL,
-  `mahasiswa_id` int(11) NOT NULL
+  `mahasiswa_id` int(11) NOT NULL,
+  `pertemuan_matakuliah_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `absensi`
 --
 
-INSERT INTO `absensi` (`id`, `tanggal_waktu`, `pertemuan`, `mahasiswa_id`) VALUES
-(2, '2019-05-12 17:30:42', 1, 7),
-(3, '2019-05-12 17:33:34', 1, 8),
-(4, '2019-05-12 17:33:47', 2, 8),
-(5, '2019-05-12 17:33:49', 3, 8),
-(6, '2019-05-12 17:33:52', 4, 8),
-(7, '2019-05-12 17:33:54', 5, 8),
-(8, '2019-05-12 17:33:57', 6, 8),
-(9, '2019-05-12 17:34:03', 3, 7),
-(10, '2019-05-12 17:34:05', 5, 7);
+INSERT INTO `absensi` (`id`, `tanggal_waktu`, `mahasiswa_id`, `pertemuan_matakuliah_id`) VALUES
+(14, '2019-05-23 04:37:11', 7, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ambil_matakuliah`
+--
+
+CREATE TABLE `ambil_matakuliah` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `mahasiswa_id` int(11) DEFAULT NULL,
+  `matakuliah_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ambil_matakuliah`
+--
+
+INSERT INTO `ambil_matakuliah` (`id`, `mahasiswa_id`, `matakuliah_id`) VALUES
+(2, 7, 11),
+(3, 8, 11),
+(11, 18, 11);
 
 -- --------------------------------------------------------
 
@@ -60,19 +73,19 @@ CREATE TABLE `mahasiswa` (
   `id` int(11) NOT NULL,
   `nrp` varchar(30) NOT NULL,
   `nama` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `matakuliah_id` int(11) DEFAULT NULL
+  `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `password`, `matakuliah_id`) VALUES
-(7, '164', 'hilmi', 'hilmi', 11),
-(8, '057', 'naufalpf', 'naufalpf', 13),
-(9, '050', 'akram', 'akram', 13),
-(10, '058', 'test', 'test', NULL);
+INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `password`) VALUES
+(7, '164', 'hilmi', 'hilmi'),
+(8, '057', 'naufalpf', 'naufalpf'),
+(9, '050', 'akram', 'akram'),
+(10, '058', 'test', 'test'),
+(18, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -85,18 +98,19 @@ CREATE TABLE `matakuliah` (
   `nama` varchar(30) NOT NULL,
   `jumlah_pertemuan` int(11) NOT NULL,
   `semester` varchar(30) NOT NULL,
-  `kelas` varchar(30) NOT NULL
+  `kelas` varchar(30) NOT NULL,
+  `kode_matkul` varchar(11) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `matakuliah`
 --
 
-INSERT INTO `matakuliah` (`id`, `nama`, `jumlah_pertemuan`, `semester`, `kelas`) VALUES
-(11, 'PBKK', 16, '6', 'G'),
-(12, 'PBKK', 16, '6', 'H'),
-(13, 'IMK', 16, '6', 'H'),
-(14, 'bdt', 16, '6', 'H');
+INSERT INTO `matakuliah` (`id`, `nama`, `jumlah_pertemuan`, `semester`, `kelas`, `kode_matkul`) VALUES
+(11, 'PBKK', 16, '6', 'G', 'IF999'),
+(12, 'PBKK', 16, '6', 'H', 'IF777'),
+(13, 'IMK', 16, '6', 'H', 'IF888'),
+(14, 'bdt', 16, '6', 'H', 'if666');
 
 -- --------------------------------------------------------
 
@@ -107,8 +121,8 @@ INSERT INTO `matakuliah` (`id`, `nama`, `jumlah_pertemuan`, `semester`, `kelas`)
 CREATE TABLE `pertemuan_matakuliah` (
   `id` int(11) NOT NULL,
   `pertemuan` int(11) NOT NULL,
-  `jam_mulai` time NOT NULL,
-  `jam_selesai` time NOT NULL,
+  `jam_mulai` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `jam_selesai` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `ruangan` varchar(30) NOT NULL,
   `matakuliah_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -118,13 +132,7 @@ CREATE TABLE `pertemuan_matakuliah` (
 --
 
 INSERT INTO `pertemuan_matakuliah` (`id`, `pertemuan`, `jam_mulai`, `jam_selesai`, `ruangan`, `matakuliah_id`) VALUES
-(2, 1, '09:30:00', '12:30:00', '105a', 11),
-(3, 1, '12:30:00', '15:00:00', '105a', 12),
-(4, 1, '12:30:00', '15:00:00', '105a', 13),
-(5, 1, '12:30:00', '15:00:00', '105a', 14),
-(6, 2, '12:30:00', '15:00:00', '105a', 14),
-(7, 3, '12:30:00', '15:00:00', '105a', 14),
-(8, 4, '12:30:00', '15:00:00', '105a', 14);
+(2, 1, '2019-05-21 17:00:00', '2019-05-23 17:00:00', '105a', 11);
 
 --
 -- Indexes for dumped tables
@@ -135,14 +143,22 @@ INSERT INTO `pertemuan_matakuliah` (`id`, `pertemuan`, `jam_mulai`, `jam_selesai
 --
 ALTER TABLE `absensi`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `mahasiswa_id` (`mahasiswa_id`);
+  ADD KEY `mahasiswa_id` (`mahasiswa_id`),
+  ADD KEY `pertemuan_matakuliah_id` (`pertemuan_matakuliah_id`);
+
+--
+-- Indexes for table `ambil_matakuliah`
+--
+ALTER TABLE `ambil_matakuliah`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `mahasiswa_id` (`mahasiswa_id`),
+  ADD KEY `matakuliah_id` (`matakuliah_id`);
 
 --
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `matakuliah_id` (`matakuliah_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `matakuliah`
@@ -165,13 +181,19 @@ ALTER TABLE `pertemuan_matakuliah`
 -- AUTO_INCREMENT for table `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `ambil_matakuliah`
+--
+ALTER TABLE `ambil_matakuliah`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `matakuliah`
@@ -183,7 +205,7 @@ ALTER TABLE `matakuliah`
 -- AUTO_INCREMENT for table `pertemuan_matakuliah`
 --
 ALTER TABLE `pertemuan_matakuliah`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -193,15 +215,15 @@ ALTER TABLE `pertemuan_matakuliah`
 -- Constraints for table `absensi`
 --
 ALTER TABLE `absensi`
-  ADD CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`mahasiswa_id`) REFERENCES `mahasiswa` (`id`),
-  ADD CONSTRAINT `absensi_ibfk_2` FOREIGN KEY (`mahasiswa_id`) REFERENCES `mahasiswa` (`id`);
+  ADD CONSTRAINT `absensi_ibfk_2` FOREIGN KEY (`pertemuan_matakuliah_id`) REFERENCES `pertemuan_matakuliah` (`id`),
+  ADD CONSTRAINT `absensi_ibfk_3` FOREIGN KEY (`mahasiswa_id`) REFERENCES `mahasiswa` (`id`);
 
 --
--- Constraints for table `mahasiswa`
+-- Constraints for table `ambil_matakuliah`
 --
-ALTER TABLE `mahasiswa`
-  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`matakuliah_id`) REFERENCES `matakuliah` (`id`),
-  ADD CONSTRAINT `mahasiswa_ibfk_2` FOREIGN KEY (`matakuliah_id`) REFERENCES `matakuliah` (`id`);
+ALTER TABLE `ambil_matakuliah`
+  ADD CONSTRAINT `ambil_matakuliah_ibfk_1` FOREIGN KEY (`mahasiswa_id`) REFERENCES `mahasiswa` (`id`),
+  ADD CONSTRAINT `ambil_matakuliah_ibfk_2` FOREIGN KEY (`matakuliah_id`) REFERENCES `matakuliah` (`id`);
 
 --
 -- Constraints for table `pertemuan_matakuliah`
